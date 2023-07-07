a58a7a60de9a435cec29da9da5addcbd6a8a0456
Nyakno Efiom <nyaknoefiom@yahoo.com>
2023-07-07 04:14:43 +0000

    python annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-from typing import Tuple
+"""
+12. Type Checking
+"""
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    zoomed_in: Tuple[int, ...] = tuple(
-        item for item in lst
-        for i in range(factor)
-    )
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+            item for item in lst
+            for i in range(factor)
+            ]
     return zoomed_in
 
 
@@ -14,4 +19,3 @@ array = (12, 72, 91)
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
-
